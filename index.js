264c1647f66674d0d3252ae1e356f81483c9d3cf
Jessica Alves <jessicazz0407@gmail.com>
2022-12-13 12:49:47 -0300

    adição de mascara tel, com JS

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const menu = document.querySelector('.menu');
-const NavMenu = document.querySelector('.nav-menu');
+const handlePhone = (event) => {
+    let input = event.target
+    input.value = phoneMask(input.value)
+}
 
-menu.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-    NavMenu.classList.toggle('ativo');
-})
+const phoneMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g, '')
+    value = value.replace(/(\d{2})(\d)/, "($1) $2")
+    value = value.replace(/(\d)(\d{4})$/, "$1-$2")
+    return value
+}
